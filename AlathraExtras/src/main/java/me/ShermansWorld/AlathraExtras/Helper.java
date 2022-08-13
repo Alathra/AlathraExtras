43baa00e7fe6c79b050ea44f5839810be23f02ee
ShermansWorld <77093412+ShermansWorld@users.noreply.github.com>
2022-08-12 20:31:29 -0400

    Add files via upload

---
+++ AlathraExtras/src/main/java/me/ShermansWorld/AlathraExtras/Helper.java
@@ -0,0 +1,15 @@
+package me.ShermansWorld.AlathraExtras;
+
+import org.bukkit.ChatColor;
+
+public class Helper
+{
+    public static String color(final String input) {
+        return ChatColor.translateAlternateColorCodes('&', input);
+    }
+    
+    public static String Chatlabel() {
+        return color("&6[&aAlathraExtras&6]&r ");
+    }
+    
+}
---
+++ AlathraExtras/src/main/java/me/ShermansWorld/AlathraExtras/voting/VotingListener.java
@@ -0,0 +1,57 @@
+package me.ShermansWorld.AlathraExtras.voting;
+
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.bencodez.votingplugin.VotingPluginMain;
+import com.bencodez.votingplugin.events.PlayerVoteEvent;
+import com.bencodez.votingplugin.user.UserManager;
+
+import me.ShermansWorld.AlathraExtras.CustomListeners;
+import me.ShermansWorld.AlathraExtras.Main;
+public class VotingListener implements Listener {
+	
+public static VotingPluginMain votingPlugin = null;
+public static UserManager votingUserManager = null;
+public static ArrayList<Player> playersWhoVoted = new ArrayList<Player>();
+
+	public static void initVotingPlugin() {
+		if (votingPlugin == null) {
+			votingPlugin = JavaPlugin.getPlugin(VotingPluginMain.class);
+			votingUserManager = votingPlugin.getVotingPluginUserManager();
+		}
+	}
+	
+	@EventHandler
+	public static void onVote(PlayerVoteEvent e) {
+		
+		initVotingPlugin();
+		
+		Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), new Runnable() {
+		    @Override
+		    public void run() {
+		    	CustomListeners.checkForAllSitesVoted();
+		    }
+		}, 20L); //20 Tick (1 Second) delay before run() is called
+				
+		try {
+			Player p = Bukkit.getPlayer(e.getPlayer());
+			if (playersWhoVoted.isEmpty()) {
+				playersWhoVoted.add(p);
+			} else {
+				if (!(playersWhoVoted.contains(p))) {
+					playersWhoVoted.add(p);
+				}
+			}
+		} catch(NullPointerException err) {
+			
+		}
+		
+	}
+	
+}
