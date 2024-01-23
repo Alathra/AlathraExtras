2e0e98565a0f9a0fcb4141397fb7cabf16bf71ec
Xavier Beatrice <58835993+Xavbeat03@users.noreply.github.com>
2024-01-22 19:49:51 -0500

    Update src/main/java/me/ShermansWorld/AlathraExtras/npcs/NPCListener.java
    Co-authored-by: darksaid98 <darksaid9889@gmail.com>

--- src/main/java/me/ShermansWorld/AlathraExtras/npcs/NPCListener.java
+++ src/main/java/me/ShermansWorld/AlathraExtras/npcs/NPCListener.java
@@ -5,7 +5,7 @@ import org.bukkit.event.EventHandler;
 
 import org.bukkit.event.Listener;
 
-public class NPCListener  implements Listener {
+public class NPCListener implements Listener {
 
     @EventHandler
     public void NPCSpawnListener(NPCSpawnEvent e) {
