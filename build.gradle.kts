fb932d2223e69341c08d71331e823efaf3a1e3ed
ShermansWorld <77093412+ShermansWorld@users.noreply.github.com>
2024-01-27 23:07:55 -0500

    fix: siege engines dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,7 +92,7 @@ dependencies {
     compileOnly("dev.cubxity.plugins:unifiedmetrics-api:0.3.8")
     compileOnly("nl.rutgerkok:blocklocker:1.10.4")
     compileOnly("io.lumine:Mythic-Dist:5.4.1")
-    compileOnly(files("lib/SiegeEngines-0.7.5.jar"))
+    compileOnly(files("lib/SiegeEngines-0.7.6.jar"))
 }
 
 tasks {
