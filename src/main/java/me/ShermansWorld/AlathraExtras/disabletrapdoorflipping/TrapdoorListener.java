4fb345dc45993c6ba3941163b3c97b9fca78f76f
Xavier Beatrice <Xavbeat03@gmail.com>
2024-02-14 22:57:47 -0500

    Registering the trapdoor listener.

--- src/main/java/me/ShermansWorld/AlathraExtras/disabletrapdoorflipping/TrapdoorListener.java
+++ src/main/java/me/ShermansWorld/AlathraExtras/disabletrapdoorflipping/TrapdoorListener.java
@@ -22,7 +22,7 @@ public class TrapdoorListener implements Listener {
         // Checks if the action is right click, and the right click is at a block.
         if(e.getAction() == Action.RIGHT_CLICK_BLOCK
             && e.getClickedBlock() != null
-            && e.getClickedBlock().getType().toString().toLowerCase().contains("trapdoor")
+            && MaterialTags.TRAPDOORS.isTagged(e.getClickedBlock().getType())
             && e.getClickedBlock().getWorld().getName().equals("world")
             && e.getPlayer().getGameMode() != GameMode.CREATIVE
         ) // If so, cancel the interaction.
