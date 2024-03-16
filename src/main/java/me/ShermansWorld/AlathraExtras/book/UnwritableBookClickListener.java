167ecc7fc6407214348d16c1beaffbcd61546f36
xavierb <Xavbeat03@gmail.com>
2024-03-16 14:32:32 -0400

    fix: Added alternative cost, now works with book and quills.

--- src/main/java/me/ShermansWorld/AlathraExtras/book/UnwritableBookClickListener.java
+++ src/main/java/me/ShermansWorld/AlathraExtras/book/UnwritableBookClickListener.java
@@ -2,6 +2,7 @@ package me.ShermansWorld.AlathraExtras.book;
 
 import com.github.milkdrinkers.colorparser.ColorParser;
 import me.ShermansWorld.AlathraExtras.AlathraExtras;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Lectern;
@@ -22,8 +23,7 @@ public class UnwritableBookClickListener implements Listener {
     @EventHandler
     public void onInteract(InventoryOpenEvent event){
 
-        //TODO Come up with alternative cost for Book duplication (new item? or feather?)
-        //TODO Make sure Books and Quills can be duplicated as well
+        // TODO Fix bug where original book gets the 'Copy' Tag
 
         // If player is not a Bukkit Player return
         if(!(event.getPlayer() instanceof Player player)) return;
@@ -31,40 +31,45 @@ public class UnwritableBookClickListener implements Listener {
         if(event.getInventory() instanceof LecternInventory lecternInventory &&
             (!lecternInventory.isEmpty())) {
                 PlayerInventory playerInventory = player.getInventory();
-                if (playerInventory.getItemInMainHand().getType() == Material.BOOK) {
+                if (playerInventory.getItemInMainHand().getType() == Material.BOOK && playerInventory.getItemInOffHand().getType() == Material.GLOW_INK_SAC) {
                     if (lecternInventory.getBook() == null) return;
                     // Get the book from the lectern
                     ItemStack writtenBook = lecternInventory.getBook();
-
                     ItemMeta writtenBookMeta = writtenBook.getItemMeta();
-                    if(writtenBook.getItemMeta().lore() != null)
-                        writtenBookMeta.lore().add(ColorParser.of("<yellow>Copy</yellow>").build());
-                    else
-                        writtenBookMeta.lore(Collections.singletonList(ColorParser.of("<yellow>Copy</yellow>").build()));
-                    writtenBook.setItemMeta(writtenBookMeta);
 
+                    ItemStack duplicateBook = writtenBook.clone();
+                    ItemMeta duplicateBookMeta = duplicateBook.getItemMeta();
+                    if(duplicateBook.getType() == Material.WRITABLE_BOOK) {
+                        if (duplicateBook.getItemMeta().lore() != null)
+                            duplicateBookMeta.lore().add(ColorParser.of("<yellow>Copy</yellow>").build());
+                        else
+                            duplicateBookMeta.lore(Collections.singletonList(ColorParser.of("<yellow>Copy</yellow>").build()));
+                    }
+                    duplicateBook.setItemMeta(duplicateBookMeta);
                     // Make sure its just one in the itemstack
-                    writtenBook.asOne();
+                    duplicateBook.asOne();
                     // Get the book in the players hand
                     ItemStack book = playerInventory.getItemInMainHand();
                     // Only get one
                     book.asOne();
+                    // Get the glow ink sack in the offhand
+                    ItemStack glowInkSack = playerInventory.getItemInOffHand();
+                    //Only get one
+                    glowInkSack.asOne();
                     // Schedule a task to remove the book, give them the item and add the new book.
+
+                    writtenBook.setItemMeta(writtenBookMeta);
                     Bukkit.getScheduler().scheduleSyncDelayedTask(AlathraExtras.getInstance(), () ->
                     {
                         // Check the players values again so that nothing breaks.
-                        if (playerInventory.getItemInMainHand().isSimilar(book)) {
-                            // Actually remove the book, and give the written book
+                        if (playerInventory.getItemInMainHand().isSimilar(book) && playerInventory.getItemInOffHand().isSimilar(glowInkSack)) {
+                            // Actually remove the book and the glow ink sack, and give the written book
                             playerInventory.getItemInMainHand().setAmount(playerInventory.getItemInMainHand().getAmount() - 1);
-                            playerInventory.addItem(writtenBook);
+                            playerInventory.getItemInOffHand().setAmount(playerInventory.getItemInOffHand().getAmount()-1);
+                            playerInventory.addItem(duplicateBook);
                         }
                     }, 1L);
-
-
                 }
-
         }
     }
-
-
 }
