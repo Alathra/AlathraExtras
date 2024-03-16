5888459e00bbb2f1a6a352e8301c3da92799fe61
xavierb <Xavbeat03@gmail.com>
2024-03-16 04:25:29 -0400

    fix: Removed exp cost, going to look into alternative cost.

--- src/main/java/me/ShermansWorld/AlathraExtras/book/UnwritableBookClickListener.java
+++ src/main/java/me/ShermansWorld/AlathraExtras/book/UnwritableBookClickListener.java
@@ -1,5 +1,6 @@
 package me.ShermansWorld.AlathraExtras.book;
 
+import com.github.milkdrinkers.colorparser.ColorParser;
 import me.ShermansWorld.AlathraExtras.AlathraExtras;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -7,43 +8,63 @@ import org.bukkit.block.Lectern;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.LecternInventory;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Collections;
 
 public class UnwritableBookClickListener implements Listener {
 
     @EventHandler
-    public void onInteract(PlayerInteractEvent event){
-        // Check that the player sneak right clicked a lectern
-        Player player = event.getPlayer();
-        if(event.getAction().isRightClick() && player.isSneaking() &&
-            (event.getClickedBlock() != null && event.getClickedBlock().getType() == Material.LECTERN)){
-                Lectern lectern = (Lectern) event.getClickedBlock();
-                // Check that the lectern isn't empty and that the player is holding a book while having >50 experience points
-                if(!lectern.getInventory().isEmpty() &&
-                    (player.getInventory().getItemInMainHand().getType() == Material.BOOK &&
-                        player.getTotalExperience() > 50)){
-                        // Get the book from the lectern
-                        ItemStack writtenBook = lectern.getSnapshotInventory().getItem(lectern.getSnapshotInventory().first(Material.WRITTEN_BOOK));
-                        if(writtenBook==null) return;
-                        // Make sure its just one in the itemstack
-                        writtenBook.asOne();
-                        // Get the book in the players hand
-                        ItemStack book = player.getInventory().getItemInMainHand();
-                        // Only get one
-                        book.asOne();
-                        // Schedule a task to remove the book, give them the item and add the new book.
-                        Bukkit.getScheduler().scheduleSyncDelayedTask(AlathraExtras.getInstance(), ()->
-                        {
-                            // Check the players values again so that nothing breaks.
-                            if(player.getTotalExperience() > 50 && player.getInventory().contains(book)) {
-                                player.getInventory().remove(book);
-                                player.giveExp(-50);
-                                player.getInventory().addItem(writtenBook);
-                            }
-                        }, 1L);
+    public void onInteract(InventoryOpenEvent event){
+
+        //TODO Come up with alternative cost for Book duplication (new item? or feather?)
+        //TODO Make sure Books and Quills can be duplicated as well
+
+        // If player is not a Bukkit Player return
+        if(!(event.getPlayer() instanceof Player player)) return;
+		// Inventory is a LecternInventory
+        if(event.getInventory() instanceof LecternInventory lecternInventory &&
+            (!lecternInventory.isEmpty())) {
+                PlayerInventory playerInventory = player.getInventory();
+                if (playerInventory.getItemInMainHand().getType() == Material.BOOK) {
+                    if (lecternInventory.getBook() == null) return;
+                    // Get the book from the lectern
+                    ItemStack writtenBook = lecternInventory.getBook();
+
+                    ItemMeta writtenBookMeta = writtenBook.getItemMeta();
+                    if(writtenBook.getItemMeta().lore() != null)
+                        writtenBookMeta.lore().add(ColorParser.of("<yellow>Copy</yellow>").build());
+                    else
+                        writtenBookMeta.lore(Collections.singletonList(ColorParser.of("<yellow>Copy</yellow>").build()));
+                    writtenBook.setItemMeta(writtenBookMeta);
+
+                    // Make sure its just one in the itemstack
+                    writtenBook.asOne();
+                    // Get the book in the players hand
+                    ItemStack book = playerInventory.getItemInMainHand();
+                    // Only get one
+                    book.asOne();
+                    // Schedule a task to remove the book, give them the item and add the new book.
+                    Bukkit.getScheduler().scheduleSyncDelayedTask(AlathraExtras.getInstance(), () ->
+                    {
+                        // Check the players values again so that nothing breaks.
+                        if (playerInventory.getItemInMainHand().isSimilar(book)) {
+                            // Actually remove the book, and give the written book
+                            playerInventory.getItemInMainHand().setAmount(playerInventory.getItemInMainHand().getAmount() - 1);
+                            playerInventory.addItem(writtenBook);
+                        }
+                    }, 1L);
+
+
                 }
 
         }
     }
+
+
 }
