46d5924913019a1a32ba858b2cec12ae4b787812
xavierb <Xavbeat03@gmail.com>
2024-03-17 11:29:37 -0400

    fix: removed UUID print, and changed to Instant.now() which is supposed to be in UTC.

--- src/main/java/me/ShermansWorld/AlathraExtras/book/BookEventsListener.java
+++ src/main/java/me/ShermansWorld/AlathraExtras/book/BookEventsListener.java
@@ -9,20 +9,20 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerEditBookEvent;
 import org.bukkit.inventory.meta.BookMeta;
 
+import java.time.Instant;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
 public class BookEventsListener implements Listener {
-
+    // TODO Add UUID to Book as Some kind data
     @EventHandler
     public void onPlayerEditBook(PlayerEditBookEvent e) {
         if(e.isSigning()){
             BookMeta bookMeta = e.getPreviousBookMeta();
             List<Component> list = new java.util.ArrayList<>(Collections.emptyList());
             list.add(ColorParser.of("<white>Signed by %s</white>".formatted(e.getPlayer().getName())).build());
-            list.add(ColorParser.of("<white>%s</white>".formatted(e.getPlayer().getUniqueId().toString())).build());
-            list.add(ColorParser.of("<white>" + Date.from(Time.now()).toString() + "</white>").build());
+            list.add(ColorParser.of("<white>" + Date.from(Instant.now()).toString() + "</white>").build());
             bookMeta.lore(list);
             e.setNewBookMeta(bookMeta);
         }
