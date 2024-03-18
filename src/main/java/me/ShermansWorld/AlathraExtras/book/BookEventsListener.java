966867712d238097700257270613ee06d7059193
xavierb <Xavbeat03@gmail.com>
2024-03-18 14:31:40 -0400

    fix: Properly implemented the formatting required to show the dates in UTC, should look nicer as well

--- src/main/java/me/ShermansWorld/AlathraExtras/book/BookEventsListener.java
+++ src/main/java/me/ShermansWorld/AlathraExtras/book/BookEventsListener.java
@@ -1,28 +1,33 @@
 package me.ShermansWorld.AlathraExtras.book;
 
 import com.github.alathra.siegeengines.libs.colorparser.ColorParser;
-
-import io.lumine.mythic.bukkit.utils.time.Time;
 import net.kyori.adventure.text.Component;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerEditBookEvent;
 import org.bukkit.inventory.meta.BookMeta;
 
-import java.time.Instant;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 public class BookEventsListener implements Listener {
-    // TODO Add UUID to Book as Some kind data
     @EventHandler
     public void onPlayerEditBook(PlayerEditBookEvent e) {
         if(e.isSigning()){
             BookMeta bookMeta = e.getPreviousBookMeta();
             List<Component> list = new java.util.ArrayList<>(Collections.emptyList());
+
+            Calendar currentDate = Calendar.getInstance();
+            DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            TimeZone obj = TimeZone.getTimeZone("UTC");
+
+            formatter.setTimeZone(obj);
             list.add(ColorParser.of("<white>Signed by %s</white>".formatted(e.getPlayer().getName())).build());
-            list.add(ColorParser.of("<white>" + Date.from(Instant.now()).toString() + "</white>").build());
+            list.add(ColorParser.of("<white>" + formatter.format(currentDate.getTime()) + " UTC</white>").build());
             bookMeta.lore(list);
             e.setNewBookMeta(bookMeta);
         }
