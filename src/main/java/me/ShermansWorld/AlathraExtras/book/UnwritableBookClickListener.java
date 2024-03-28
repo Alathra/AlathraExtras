3626d90fc168578ef18d32af79f61b2f6a883692
xavierb <Xavbeat03@gmail.com>
2024-03-28 12:48:01 -0400

    fix: thinning scope and trying to fix a bug where it eats more then 1 glow ink sac

--- src/main/java/me/ShermansWorld/AlathraExtras/book/UnwritableBookClickListener.java
+++ src/main/java/me/ShermansWorld/AlathraExtras/book/UnwritableBookClickListener.java
@@ -22,50 +22,41 @@ public class UnwritableBookClickListener implements Listener {
 
     @EventHandler
     public void onInteract(InventoryOpenEvent event){
-
-        // TODO Fix bug where original book gets the 'Copy' Tag
-
         // If player is not a Bukkit Player return
         if(!(event.getPlayer() instanceof Player player)) return;
 		// Inventory is a LecternInventory
         if(event.getInventory() instanceof LecternInventory lecternInventory &&
             (!lecternInventory.isEmpty())) {
-                PlayerInventory playerInventory = player.getInventory();
-                if (playerInventory.getItemInMainHand().getType() == Material.BOOK && playerInventory.getItemInOffHand().getType() == Material.GLOW_INK_SAC) {
+
+            PlayerInventory playerInventory = player.getInventory();
+            ItemStack itemInMainHand = playerInventory.getItemInMainHand();
+            ItemStack itemInOffHand = playerInventory.getItemInOffHand();
+
+            if (itemInMainHand.getType() == Material.BOOK && itemInOffHand.getType() == Material.GLOW_INK_SAC) {
                     if (lecternInventory.getBook() == null) return;
                     // Get the book from the lectern
                     ItemStack writtenBook = lecternInventory.getBook();
                     ItemMeta writtenBookMeta = writtenBook.getItemMeta();
 
                     ItemStack duplicateBook = writtenBook.clone();
-                    ItemMeta duplicateBookMeta = duplicateBook.getItemMeta();
-                    if(duplicateBook.getType() != Material.WRITABLE_BOOK) {
-                        if (duplicateBook.getItemMeta().lore() != null)
-                            duplicateBookMeta.lore().add(ColorParser.of("<yellow>Copy</yellow>").build());
-                        else
-                            duplicateBookMeta.lore(Collections.singletonList(ColorParser.of("<yellow>Copy</yellow>").build()));
-                    }
-                    duplicateBook.setItemMeta(duplicateBookMeta);
                     // Make sure its just one in the itemstack
-                    duplicateBook.asOne();
+                    duplicateBook.setAmount(1);
                     // Get the book in the players hand
-                    ItemStack book = playerInventory.getItemInMainHand();
+                    ItemStack book = itemInMainHand;
                     // Only get one
                     book.asOne();
                     // Get the glow ink sack in the offhand
-                    ItemStack glowInkSack = playerInventory.getItemInOffHand();
-                    //Only get one
-                    glowInkSack.asOne();
+                    ItemStack glowInkSack = itemInOffHand;
                     // Schedule a task to remove the book, give them the item and add the new book.
 
                     writtenBook.setItemMeta(writtenBookMeta);
                     Bukkit.getScheduler().scheduleSyncDelayedTask(AlathraExtras.getInstance(), () ->
                     {
                         // Check the players values again so that nothing breaks.
-                        if (playerInventory.getItemInMainHand().isSimilar(book) && playerInventory.getItemInOffHand().isSimilar(glowInkSack)) {
+                        if (itemInMainHand.isSimilar(book) && itemInOffHand.isSimilar(glowInkSack)) {
                             // Actually remove the book and the glow ink sack, and give the written book
-                            playerInventory.getItemInMainHand().setAmount(playerInventory.getItemInMainHand().getAmount() - 1);
-                            playerInventory.getItemInOffHand().setAmount(playerInventory.getItemInOffHand().getAmount()-1);
+                            itemInMainHand.setAmount(itemInMainHand.getAmount() - 1);
+                            itemInOffHand.setAmount(itemInOffHand.getAmount() - 1);
                             playerInventory.addItem(duplicateBook);
                         }
                     }, 1L);
