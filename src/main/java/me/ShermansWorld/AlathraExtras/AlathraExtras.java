f9e23b6a3d459705fdd2977fb00c6166fbcd42bd
xavierb <Xavbeat03@gmail.com>
2024-04-07 14:02:46 -0400

    docs: adding some more labeling

--- src/main/java/me/ShermansWorld/AlathraExtras/AlathraExtras.java
+++ src/main/java/me/ShermansWorld/AlathraExtras/AlathraExtras.java
@@ -160,7 +160,8 @@ public class AlathraExtras extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents(new NPCListener(), this);
         this.getServer().getPluginManager().registerEvents(new TrapdoorListener(), this);
         this.getServer().getPluginManager().registerEvents(new FoodConsumeListener(), this);
-        //this.getServer().getPluginManager().registerEvents(new BookEventsListener(), this);
+        // this.getServer().getPluginManager().registerEvents(new BookEventsListener(), this);
+        // This is broken do not enable unless you have confirmed its working.
 
         initRecipeItems();
         FurnaceRecipes furnaceRecipes = new FurnaceRecipes();
